cdeb6b9e226ce853e2deaf77241003e2782220ad
himanshu999 <himanshunathani999@gmail.com>
2021-06-14 17:50:20 +0000

    Update Sidebar.Properties.js

--- editor/js/Sidebar.Properties.js
+++ editor/js/Sidebar.Properties.js
@@ -1,7 +1,5 @@
 import { UITabbedPanel } from './libs/ui.js';
 
-import { SidebarObject } from './Sidebar.Object.js';
-import { SidebarGeometry } from './Sidebar.Geometry.js';
 import { SidebarMaterial } from './Sidebar.Material.js';
 
 function SidebarProperties( editor ) {
@@ -11,10 +9,8 @@ function SidebarProperties( editor ) {
 	var container = new UITabbedPanel();
 	container.setId( 'properties' );
 
-	container.addTab( 'object', strings.getKey( 'sidebar/properties/object' ), new SidebarObject( editor ) );
-	container.addTab( 'geometry', strings.getKey( 'sidebar/properties/geometry' ), new SidebarGeometry( editor ) );
 	container.addTab( 'material', strings.getKey( 'sidebar/properties/material' ), new SidebarMaterial( editor ) );
-	container.select( 'object' );
+	container.select( 'material' );
 
 	return container;
 
