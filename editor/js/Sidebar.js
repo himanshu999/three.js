fc3095db159c2b96ef1f510875695ef1f67be08f
himanshu999 <himanshunathani999@gmail.com>
2021-06-15 16:42:55 +0000

    Update Sidebar.js

--- editor/js/Sidebar.js
+++ editor/js/Sidebar.js
@@ -18,14 +18,14 @@ function Sidebar( editor ) {
 	var scene = new UISpan().add(
 		new SidebarScene( editor ),
 		new SidebarProperties( editor ),
-		new SidebarAnimation( editor ),
-		new SidebarScript( editor )
+		/*new SidebarAnimation( editor ),
+		new SidebarScript( editor )*/
 		
 	);
 	var project = new SidebarProject( editor );
 	var settings = new SidebarSettings( editor );
 
-	//container.addTab( 'scene', strings.getKey( 'sidebar/scene' ), scene );
+	container.addTab( 'scene', strings.getKey( 'sidebar/scene' ), scene );
 	//container.addTab( 'project', strings.getKey( 'sidebar/project' ), project );
 	//container.addTab( 'settings', strings.getKey( 'sidebar/settings' ), settings );
 	container.select( 'scene' );
