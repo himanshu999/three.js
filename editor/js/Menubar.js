a963d00f80ce6e983dd5ee8e29fd91aa60f81ce2
himanshu999 <himanshunathani999@gmail.com>
2021-06-17 10:34:45 +0000

    Update Menubar.js

--- editor/js/Menubar.js
+++ editor/js/Menubar.js
@@ -1,8 +1,8 @@
 import { UIPanel } from './libs/ui.js';
 
-import { MenubarAdd } from './Menubar.Add.js';
+//import { MenubarAdd } from './Menubar.Add.js';
 import { MenubarEdit } from './Menubar.Edit.js';
-import { MenubarFile } from './Menubar.File.js';
+//import { MenubarFile } from './Menubar.File.js';
 
 
 function Menubar( editor ) {
@@ -10,9 +10,9 @@ function Menubar( editor ) {
 	var container = new UIPanel();
 	container.setId( 'menubar' );
 
-	container.add( new MenubarFile( editor ) );
+	//container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
-	container.add( new MenubarAdd( editor ) );
+	//container.add( new MenubarAdd( editor ) );
 	//container.add( new MenubarStatus( editor ) );
 
 	return container;
