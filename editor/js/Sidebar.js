e795c225988a7fdbc3b994d3e5cd55e6ec2206bf
himanshu999 <himanshunathani999@gmail.com>
2021-06-17 06:33:28 +0000

    Update Sidebar.js

--- editor/js/Sidebar.js
+++ editor/js/Sidebar.js
@@ -2,10 +2,6 @@ import { UIPanel, UISpan } from './libs/ui.js';
 
 import { SidebarScene } from './Sidebar.Scene.js';
 import { SidebarProperties } from './Sidebar.Properties.js';
-import { SidebarScript } from './Sidebar.Script.js';
-
-import { SidebarProject } from './Sidebar.Project.js';
-import { SidebarSettings } from './Sidebar.Settings.js';
 
 
 function Sidebar( editor ) {
@@ -18,15 +14,12 @@ function Sidebar( editor ) {
 	var scene = new UISpan().add(
 		/*new SidebarScene( editor ),*/
 		new SidebarProperties( editor ),
-		/*new SidebarAnimation( editor ),
-		new SidebarScript( editor )*/
+		
 		
 	);
 	
 	container.add(scene);
 	
-	var project = new SidebarProject( editor );
-	var settings = new SidebarSettings( editor );
 
 	/*container.addTab( 'scene', strings.getKey( 'sidebar/scene' ), scene );
 	//container.addTab( 'project', strings.getKey( 'sidebar/project' ), project );
