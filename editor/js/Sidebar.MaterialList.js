f59a3b5e7cb991da35c017b329a25274f2591f15
himanshu999 <himanshunathani999@gmail.com>
2021-06-20 09:36:20 +0000

    Create Sidebar.MaterialList.js

---
+++ editor/js/Sidebar.MaterialList.js
@@ -0,0 +1,52 @@
+import { UIButton, UICheckbox, UIColor, UIInput, UINumber, UIPanel, UIRow, UISelect, UIText, UITextArea } from './libs/ui.js';
+
+
+function SidebarMaterialList(editor){
+  
+  var container = new UIPanel();
+  container.setId('materiallist');
+  container.setBorderTop( '0' );
+  container.setPaddingTop( '4px' );
+  
+  container.add(new UIText('Material Library').addClass('sidebarh1'));
+	
+	container.add(new UIText('Leather').addClass('sidebarh2'));
+  
+  var materialListRow = new UIRow();
+  materialListRow.add(createMaterialView());
+  
+  container.add(materialListRow);
+  
+  /*var createHotspotButton = new UIButton('Add Hotspots').addClass('primary').onClick(() => {
+          
+	  if(editor.hotspotPoints.length === 0)
+	  alert('Click on the point where you want to place the annotation');
+	  
+	  editor.isSettingHotspot = true;		
+	  container.add(createHotspotDetailUI());
+	  
+	  
+  });
+	
+  hotspotButtonRow.add(createHotspotButton);
+  container.add(hotspotButtonRow);*/
+  
+  
+  function createMaterialView(){
+  
+    let materialView = `<div class="mat-thumb" style="background:red"><span class="mat-name">Testing</span></div>`;
+  	  
+  
+	  return materialView;
+  
+  } 	
+	
+  
+	
+  
+  
+  return container;
+
+}
+
+export { SidebarMaterialList };
