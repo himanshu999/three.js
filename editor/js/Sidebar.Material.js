9fb453db18b7e071508326d9c0895c67bf686f8c
himanshu999 <himanshunathani999@gmail.com>
2021-06-21 07:57:18 +0000

    Update Sidebar.Material.js

--- editor/js/Sidebar.Material.js
+++ editor/js/Sidebar.Material.js
@@ -243,9 +243,9 @@ function SidebarMaterial( editor ) {
 		
 		 matCategorised[currentMaterialCategory].forEach((mat, index) => {
 		 	if(mat.name === currentMaterialName) 
-				mat = material.toJSON().clone();
-			else if(index === (matCategorised[currentMaterialCategory].length - 1) && mat.name === !currentMaterialName)
-				matCategorised[currentMaterialCategory].push(material.toJSON().clone());
+				mat = material.toJSON();
+			else if(index == matCategorised[currentMaterialCategory].length - 1)
+				matCategorised[currentMaterialCategory].push(material.toJSON());
 				
 		 });
 		
